a467fbce603759c9d38d37d17048765283c9557d
Anthony J Bennett <anthonybennett@ecc-b-04vm14.rd.unr.edu>
2015-02-09 20:02:15 -0800

    finished distance and bearing

---
+++ project2/follower/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /nfs/home/anthonybennett/cs135projects/project2/follower
+# Build directory: /nfs/home/anthonybennett/cs135projects/project2/follower/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ project2/follower/follower.cpp
@@ -0,0 +1,98 @@
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Twist.h>
+
+#include <tf/transform_datatypes.h>
+
+#include <math.h>
+
+
+/************************************/
+  // place your functions here
+double distanceFormula(double x1, double y1, double x2, double y2) {
+	double d = sqrt((pow(x2-x1,2))+(pow(y2-y1,2)));
+	return d; 
+}
+
+double bearingFormula(double x1, double y1, double x2, double y2) {
+	double thetaT = atan((y2-y1)/(x2-x1));
+	thetaT = thetaT * (M_PI/180);
+	return thetaT;		
+}
+
+/************************************/
+
+
+/***
+  ignore these functions
+
+***/
+double red_x, red_y, blue_x, blue_y;
+double red_theta, blue_theta;
+
+double getRotationFromMsg( geometry_msgs::Quaternion q )
+{
+
+  double yaw;
+  yaw = tf::getYaw(q);
+  return yaw;
+}
+
+void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+  red_x = msg->pose.pose.position.x;
+  red_y = msg->pose.pose.position.y;
+  red_theta = getRotationFromMsg(msg->pose.pose.orientation);
+}
+
+void odom2Callback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+  blue_x = msg->pose.pose.position.x;
+  blue_y = msg->pose.pose.position.y;
+  blue_theta = getRotationFromMsg(msg->pose.pose.orientation);
+}
+
+
+int main( int argc, char* argv[] )
+{
+	// this code initializes a connection to the robot
+	ros::init(argc,argv,"follower");
+	ros::NodeHandle nh;
+	ros::Rate loop_rate(10);
+
+	ros::Publisher cmd_vel = nh.advertise<geometry_msgs::Twist>("/robot_1/cmd_vel",1000);
+  ros::Subscriber odom_sub = nh.subscribe("/robot_0/base_pose_ground_truth",1000,odomCallback);
+  ros::Subscriber odom2_sub = nh.subscribe("/robot_1/base_pose_ground_truth",1000,odom2Callback);
+
+	geometry_msgs::Twist cmd_vel_msg;
+	double des_vel = 1.0;
+
+	// loop forever
+	while( ros::ok() )
+	{
+		// by default, the robot will move forward at 1 meter / second
+		double lvel = des_vel;
+		
+		// by default, the robot will not turn at all
+		double rvel = 0;
+
+
+			//place your code here /***************************************/
+		if(distanceFormula(red_x, red_y, blue_x, blue_y) <= 1.5) {
+			lvel = 0.2;
+			rvel = bearingFormula(red_x, red_y, blue_x, blue_y) * 75;
+		} else {
+			lvel = des_vel;
+		}	
+		/***************************************/
+
+		// send the speeds to the robot
+		cmd_vel_msg.linear.x = lvel;
+		cmd_vel_msg.angular.z = rvel;
+		cmd_vel.publish(cmd_vel_msg);
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+
+	return 0;
+}
